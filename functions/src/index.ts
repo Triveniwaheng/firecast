d1b0fb1ec3af6763c22c62a7976fa4ef3f59b7a7
Triveni Wahengbam <triveni.wahengbam@mitel.com>
2020-04-12 11:50:37 +0530

    added method

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -2,75 +2,45 @@ import * as functions from 'firebase-functions';
 import * as admin from 'firebase-admin';
 admin.initializeApp()
 
-export const getUsersData = 
-functions.https.onRequest((request, response) => {
-    admin.firestore().collection('User').get()
-    .then(allUsers => {
-        // in this query allUsers has all the user
-    
-        const userList: any[] = []
-        // accessing all the users
-        allUsers.forEach(eachUser => {
-            // eachUser is the individual user
-            const  user = {
-                'Name': eachUser.get('name'),
-                'License Plate': eachUser.get('license')
-                // …...
-                // …..
-            }
-            userList.push(user)
-        })
-        console.log(userList)
-        response.send(userList)
+const db = admin.firestore()
+
+const REF_USER: string = 'user'
+const REF_HISTORY: string = 'history'
+
+export const createUser = functions.https.onRequest(async (request, response) => {
+    const data = request.body
+
+    await db.collection(REF_USER).add({
+        displayName: data.displayName,
+        password: data.password,
+        licenseNo: data.licenseNo,
+        currentParking: ''        
+    }).then(ref => {
+        console.log("User created: " + ref.id)
+        return response.status(200).send({'userID': ref.id})
+    }).catch(error => {
+        console.log("Can't create user: " + error)
+        return response.status(400).send(error)
     })
-    
-    .catch(error => {
-        console.log(error)
-        response.status(500).send(error)
+});
+
+export const vehicleCheckin = functions.https.onRequest(async (request,response) => {
+    const data = request.body.licenseNo
+
+    await db.collection(REF_HISTORY).add({
+        userId: data.userId,
+        licenseNo: data.licenseNo,
+        startTime: data.startTime,
+        endTime: data.endTime,
+        status: '',
+        duration: '',
+        price: '',
+        parkingSlot: ''
+    }).then(ref => {
+        console.log("History created: " + ref.id)
+        return response.status(200).send({'historyID': ref.id})
+    }).catch(error => {
+        console.log("Can't create history: " + error)
+        return response.status(400).send(error)
     })
-})
-
-export const getHistoryData = 
-functions.https.onRequest((request, response) => {
-    admin.firestore().collection('History').get()
-    .then(allHistory => {
-        // in this query allUsers has all the user
-    
-        const historyList: any[] = []
-        // accessing all the users
-        allHistory.forEach(eachUser => {
-            // eachUser is the individual user
-            const  history = {
-                'User Id': eachUser.get('userId'),
-                'Duration': eachUser.get('duration'),
-                'Start Time': eachUser.get('startTime'),
-                'End Time': eachUser.get('endTime')
-                // …..
-            }
-            historyList.push(history)
-        })
-        console.log(historyList)
-        response.send(historyList)
-    })
-    
-    .catch(error => {
-        console.log(error)
-        response.status(500).send(error)
-    })
-})
-
-
-
-
-//export const getUsers = functions.https.onRequest((request, response) => {
-  //admin.firestore().doc('').get()
-  //.then(snapshot => {
-    //  const data = snapshot.data()
-      //console.log(data)
-      //response.send(data)
-  //})
-  //.catch(error => {
-    //  console.log(error)
-      //response.status(500).send(error)
-  //})
-//});
+});
