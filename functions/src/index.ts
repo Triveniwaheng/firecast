1320d18f00538e8ea5e229ef96da9fb0f7159125
Waikhom,Bebikananda <Bebikananda.Waikhom@Cerner.com>
2020-04-12 12:57:40 +0530

    added: vehicleCheckin

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -3,9 +3,11 @@ import * as admin from 'firebase-admin';
 admin.initializeApp()
 
 const db = admin.firestore()
+const timestamp = admin.firestore.Timestamp
 
 const REF_USER: string = 'user'
 const REF_HISTORY: string = 'history'
+
 
 export const createUser = functions.https.onRequest(async (request, response) => {
     const data = request.body
@@ -14,7 +16,7 @@ export const createUser = functions.https.onRequest(async (request, response) =>
         displayName: data.displayName,
         password: data.password,
         licenseNo: data.licenseNo,
-        currentParking: ''        
+        currentParking: null      
     }).then(ref => {
         console.log("User created: " + ref.id)
         return response.status(200).send({'userID': ref.id})
@@ -24,18 +26,42 @@ export const createUser = functions.https.onRequest(async (request, response) =>
     })
 });
 
+
 export const vehicleCheckin = functions.https.onRequest(async (request,response) => {
-    const data = request.body.licenseNo
+    const licenseNo = request.body.licenseNo
+
+    console.log("licenseNo: " + licenseNo)
+
+    let user: any
+    await db.collection(REF_USER).where('licenseNo', '==', licenseNo).get().then(userSnapshot => {
+        if (userSnapshot.empty) {
+            return response.status(404).send({
+                'status': 404,
+                'message': 'user not found'
+            })
+        }
+        userSnapshot.forEach(userDoc => {
+            user = userDoc
+            return
+        })
+        return
+    })
+
+    console.log("user found: " + {
+        "userId": user.id,
+        "displayName": user.get('displayName'),
+        "licenseNo": user.get("licenseNo")
+    })
 
     await db.collection(REF_HISTORY).add({
-        userId: data.userId,
-        licenseNo: data.licenseNo,
-        startTime: data.startTime,
-        endTime: data.endTime,
-        status: '',
-        duration: '',
-        price: '',
-        parkingSlot: ''
+        userId: user.id,
+        licenseNo: licenseNo,
+        startTime: timestamp.fromDate(new Date()),
+        endTime: null,
+        status: null,
+        duration: null,
+        price: null,
+        parkingSlot: null
     }).then(ref => {
         console.log("History created: " + ref.id)
         return response.status(200).send({'historyID': ref.id})
